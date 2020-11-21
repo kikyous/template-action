e73408c88d078eed5842c0140d711ed42f9fcd33
chenyunjie <kikyous@163.com>
2020-11-21 21:29:08 +0800

    add post-run input

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
 import * as core from '@actions/core'
 import {context} from '@actions/github'
 import * as doT from "dot"
+import {exec} from "child_process"
 
 function run() {
   try {
     const template: string = core.getInput('template')
+    const cmd: string = core.getInput('post-run')
+
     const tempFn = doT.template(template);
     const resultText = tempFn(context);
 
     core.setOutput('content', resultText)
+    if (cmd) {
+      const cmdTempFn = doT.template(cmd);
+      const cmdText = cmdTempFn({output: resultText})
+      exec(cmdText)
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
