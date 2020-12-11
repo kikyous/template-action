e845d6168240b847d255546d7a84eb2f868587e4
chenyunjie <kikyous@163.com>
2020-12-11 19:14:12 +0800

    add require in renderContext

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,37 @@
 import * as core from '@actions/core'
 import {context} from '@actions/github'
 import {render, renderFile, Options} from 'ejs'
-import {exec} from "child_process"
+import {exec} from 'child_process'
 import {join} from 'path'
 
-const renderOptions: Options & { async: false } = {
+const renderOptions: Options & {async: false} = {
   outputFunctionName: 'echo',
   async: false
 }
 
 async function run() {
   try {
-    let template: string = core.getInput('template');
-    const cmd: string = core.getInput('post-run');
-    const templatePath: string = core.getInput('template-path');
-    let resultText: string;
+    let template: string = core.getInput('template')
+    const cmd: string = core.getInput('post-run')
+    const templatePath: string = core.getInput('template-path')
+    let resultText: string
+
+    const renderContext = {context: context, require: require}
 
     if (templatePath) {
-      const fullPath = join(process.env.GITHUB_WORKSPACE || '', templatePath);
-      resultText = await renderFile(fullPath, {context: context}, renderOptions)
+      const fullPath = join(process.env.GITHUB_WORKSPACE || '', templatePath)
+      resultText = await renderFile(fullPath, renderContext, renderOptions)
     } else {
-      resultText = render(template, {context: context}, renderOptions)
+      resultText = render(template, renderContext, renderOptions)
     }
 
     core.setOutput('content', resultText)
     if (cmd) {
       const cmdText = render(cmd, {output: resultText, context: context})
-      exec(cmdText, function(error, stdout, stderr){
+      exec(cmdText, function (error, stdout, stderr) {
         if (error) {
-          console.error(`error: ${error}`);
-          return;
+          console.error(`error: ${error}`)
+          return
         }
         core.info(stdout)
         core.error(stderr)
